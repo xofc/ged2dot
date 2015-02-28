c90eb5b50fad0dcb9f56b2b8205686aa0cb75be1
Christophe Cattelain <xof@skynet.be>
2015-02-28 11:54:53 +0100

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ged2dot
 Genealogy : Creates a simple Graphviz .dot file from a GEDCOM file.
+
+This simple PERL script generates a simple .dot file from de .ged file.
+
+Usage is :
+   $ ./ged2dot.pl < x.ged > x.dot
+or
+   $ ./ged2dot.pl < x.ged |dot -Tps -o x.ps
+
+'Individual' nodes, I*, are labelled after the NAME records
+'Family' nodes, F*, are implicitly created by FAMC & FAMS records
+FAMC records create F->I relations
+FAMS records create I->F relations
+
+'@' are removed because dot(1) doesn't like them.
+
+This is just a quick&dirty hack...
+
+See also:
+ - https://en.wikipedia.org/wiki/GEDCOM (file format)
+ - https://en.wikipedia.org/wiki/GeneWeb (genealogy software)
+ - https://en.wikipedia.org/wiki/Graphviz (graphing software)
+ - https://en.wikipedia.org/wiki/DOT_%28graph_description_language%29
+
+
---
+++ ged2dot.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+# creates a simple (Graphviz) .dot file from a GEDCOM file
+# input : STDIN  ; output : STDOUT
+# usage is './ged2dot.pl < x.ged > x.dot'
+# or './ged2dot.pl < x.ged |dot -Tps -o x.ps'
+#  (when converting to .pdf, use 'identify x.ps
+#	and ps2pdf -dDEVICEWIDTHPOINTS=2800 -dDEVICEHEIGHTPOINTS=1024 x.ps x.pdf'
+#
+sub wash{ # removes '@' from node names
+	my $x = $_[0];
+	$x =~ s/\@//g;
+	return($x);
+	}
+print "digraph G {\n";
+while ($line = <STDIN>)
+	{
+	if ($line =~ /^0 (.*) INDI/)
+		{
+		$node_indi = wash($n);
+		}
+	if ($line =~ /^1 NAME (.*?) .*\/(.*)\//)
+		{ # 1 NAME James Dewey /Watson/ --> James Watson
+		print "  $node_indi [label=\"$1 $2\"]\n";
+		}
+	if ($line =~ /^1 FAMC (.*)$/)
+		{
+		printf("    %s -> %s\n", wash($1), $node_indi);
+		}
+	if ($line =~ /^1 FAMS (.*)$/)
+		{
+		printf("    %s -> %s\n", $node_indi, wash($1));
+		}
+	}
+print "}\n";
